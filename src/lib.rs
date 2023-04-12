220005663864d059a9dfbafb35e10964426974cb
Max Kofler <kofler.max.dev@gmail.com>
2023-04-12 17:09:11 +0200

    Improved bindgen

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,24 @@
 
+#[repr(C)]
+pub struct LeafConfig {
+    loglevel: LogLevel,
+    number: u8,
+}
+
+#[repr(C)]
+pub enum LogLevel {
+    Verbose,
+    Superverbose,
+    Ultraverbose
+}
+
 #[no_mangle]
-pub extern "C" fn hello() {
-    println!("Hello from the leaf library!");
+pub extern "C" fn hello(config: &LeafConfig) {
+    println!("Hello from the leaf library, your number is {}", config.number);
+
+    match config.loglevel{
+        LogLevel::Verbose => println!("Verbose logging enabled!"),
+        LogLevel::Superverbose => println!("Superverbose logging enabled!"),
+        LogLevel::Ultraverbose => println!("Ultraverbose logging enabled!"),
+    }
 }
