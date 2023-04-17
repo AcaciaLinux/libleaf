4836a43ef511d067df4142c00a7c80ce808e0954
Max Kofler <kofler.max.dev@gmail.com>
2023-04-17 12:54:21 +0200

    Add println to pbar.rs

--- src/pbar.rs
+++ src/pbar.rs
@@ -1,7 +1,15 @@
-use indicatif::{ProgressBar, ProgressStyle};
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
+
+use once_cell::sync::Lazy;
+
+static mut MULTI_BAR: Lazy<MultiProgress> = Lazy::new(|| MultiProgress::new());
 
 pub enum Template {
     Download,
+}
+
+pub fn println<I: AsRef<str>>(msg: I) -> std::io::Result<()> {
+    unsafe { MULTI_BAR.println(msg) }
 }
 
 pub fn create_bar(template: Template) -> Option<ProgressBar> {
@@ -18,5 +26,5 @@ pub fn create_bar(template: Template) -> Option<ProgressBar> {
 
     bar.set_style(style);
 
-    Some(bar)
+    Some(unsafe { MULTI_BAR.add(bar) })
 }
