a9186d548667abe5e0ba9b21cd9635c75386b002
Max Kofler <kofler.max.dev@gmail.com>
2023-04-18 18:07:51 +0200

    Added config

---
+++ src/config.rs
@@ -0,0 +1,54 @@
+pub mod dirs;
+
+use std::path::PathBuf;
+
+/// The configuration leaf should process
+#[repr(C)]
+#[derive(Clone)]
+pub struct Config {
+    /// The loglevel to use
+    pub loglevel: LogLevel,
+
+    /// How many parallel downloads should be performed if possible
+    pub download_workers: usize,
+
+    /// The root directory leaf should work on (default: `/`)
+    pub root: Option<PathBuf>,
+
+    /// The directory leaf should store its configs in (default: `/etc/leaf/`)
+    pub config_dir: Option<PathBuf>,
+    /// The directory leaf should look for and store the mirror files (default: `/etc/leaf/mirrors/`)
+    pub mirrors_dir: Option<PathBuf>,
+
+    /// The directory leaf should store its caches in (default: `/var/cache/leaf/`)
+    pub cache_dir: Option<PathBuf>,
+    /// The directory leaf should use to cache its downloads (default: `/var/cache/leaf/download/`)
+    pub download_dir: Option<PathBuf>,
+    /// The directory leaf should use to cache its packages (default: `/var/cache/leaf/package/`)
+    pub packages_dir: Option<PathBuf>,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Config {
+            loglevel: LogLevel::Default,
+            download_workers: 5,
+            root: None,
+            config_dir: None,
+            mirrors_dir: None,
+            cache_dir: None,
+            download_dir: None,
+            packages_dir: None,
+        }
+    }
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Default)]
+pub enum LogLevel {
+    #[default]
+    Default,
+    Verbose,
+    Superverbose,
+    Ultraverbose,
+}
---
+++ src/config/dirs.rs
@@ -0,0 +1,64 @@
+use std::path::{Path, PathBuf};
+use crate::Config;
+
+impl Config {
+    /// Returns the root directory leaf should work in
+    /// 
+    /// Default: `/`
+    pub fn get_root(&self) -> &Path {
+        match &self.root {
+            Some(p) => &p,
+            None => Path::new("/"),
+        }
+    }
+
+    /// Returns the directory where leaf should put its configs (overrides the `root_dir` default)
+    /// 
+    /// Default: `root_dir/etc/leaf`
+    pub fn get_config_dir(&self) -> PathBuf {
+        match &self.config_dir {
+            Some(p) => PathBuf::from(p),
+            None => self.get_root().join("etc").join("leaf"),
+        }
+    }
+
+    /// Returns the directory leaf should look for and store the mirror files (overrides the `config_dir` default)
+    /// 
+    /// Default: `config_dir/mirrors`
+    pub fn get_mirrors_dir(&self) -> PathBuf {
+        match &self.mirrors_dir {
+            Some(p) => PathBuf::from(p),
+            None => self.get_config_dir().join("mirrors"),
+        }
+    }
+
+    /// Returns the directory where leaf should put its caches (overrides the `root_dir` default)
+    /// 
+    /// Default: `root_dir/var/cache/leaf`
+    pub fn get_cache_dir(&self) -> PathBuf {
+        match &self.cache_dir {
+            Some(p) => PathBuf::from(p),
+            None => self.get_root().join("var").join("cache").join("leaf"),
+        }
+    }
+
+    /// Returns the directory where leaf should put its download caches (overrides the `cache_dir` default)
+    /// 
+    /// /// Default: `cache_dir/download`
+    pub fn get_download_dir(&self) -> PathBuf {
+        match &self.download_dir {
+            Some(p) => PathBuf::from(p),
+            None => self.get_cache_dir().join("download"),
+        }
+    }
+
+    /// Returns the directory where leaf should put its package caches (overrides the `cache_dir` default)
+    /// 
+    /// Default: `cache_dir/package`
+    pub fn get_packages_dir(&self) -> PathBuf {
+        match &self.packages_dir {
+            Some(p) => PathBuf::from(p),
+            None => self.get_cache_dir().join("package"),
+        }
+    }
+}
