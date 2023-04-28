8dc32492f47c503537a50eee39f8762d3bb5960f
Max Kofler <kofler.max.dev@gmail.com>
2023-04-28 13:50:52 +0200

    actions: Add load_mirrors()

--- src/actions.rs
+++ src/actions.rs
@@ -41,6 +41,18 @@ pub fn update(config: &Config, mirrors: &Vec<Mirror>) -> Result<(), Vec<LError>>
     }
 }
 
+/// Loads the cached mirror file of every mirror
+/// # Arguments
+/// * `config` - The configuration to use
+/// * `mirrors` - The mirrors to update
+pub fn load_mirrors(config: &Config, mirrors: &mut Vec<Mirror>) -> Result<(), LError> {
+    for mirror in mirrors.iter_mut() {
+        mirror.load(&config)?;
+    }
+
+    Ok(())
+}
+
 pub fn download_packages(
     config: &Config,
     packages: &Vec<Arc<PackageVariant>>,
