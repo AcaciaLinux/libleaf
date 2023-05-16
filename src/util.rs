c96f76e6d5fbc9dc9b205b35273bf31ab519f159
Max Kofler <kofler.max.dev@gmail.com>
2023-05-16 09:59:58 +0200

    util::dependencies
    This new module inside the util module provides functions for working
    with dependencies.

--- src/util.rs
+++ src/util.rs
@@ -1,8 +1,7 @@
 //! Some utility functions to aid the management of the leaf package manager
 
-use crate::mirror::{self, Mirror};
-use crate::package::{Package, PackageVariant};
-use crate::{config::Config, error::*, package::remote::*};
+use crate::package::Package;
+use crate::{config::Config, error::*};
 use serde::{Deserialize, Deserializer};
 use std::fs::File;
 use std::path::{Path, PathBuf};
@@ -11,6 +10,7 @@ use std::{fmt::Display, fs::create_dir_all, str::FromStr};
 use tar::Archive;
 use xz::read::XzDecoder;
 
+pub mod dependencies;
 pub mod fs;
 pub mod hash;
 
@@ -47,92 +47,14 @@ pub fn find_package<T: Package>(name: &str, list: &[Arc<T>]) -> Option<Arc<T>> {
         .cloned()
 }
 
-/// Resolves the dependencies of the supplied package by using packages avialable in the pool
+/// Searches for a package that has a matching hash in the supplied list
 /// # Arguments
-/// * `package` - The package to resolve the dependencies of
-/// * `pool` - The pool to search in
-pub fn resolve_dependencies_pool(
-    package: Arc<PackageVariant>,
-    pool: &[Arc<PackageVariant>],
-) -> Result<Arc<PackageVariant>, LError> {
-    let mut new_package = package.as_ref().clone();
-
-    let mut new_deps: Vec<Arc<PackageVariant>> = Vec::new();
-    for dep in new_package.get_dependencies().get_unresolved()? {
-        let dependency = match find_package(dep, pool) {
-            Some(d) => d,
-            None => return Err(LError::new(LErrorClass::PackageNotFound, dep)),
-        };
-
-        let package = match &dependency.get_dependencies() {
-            Dependencies::Resolved(_) => dependency.clone(),
-            Dependencies::Unresolved(_) => resolve_dependencies_pool(dependency, pool)?,
-        };
-
-        new_deps.push(package)
-    }
-
-    new_package.set_dependencies(Dependencies::Resolved(new_deps));
-
-    Ok(Arc::new(new_package))
-}
-
-/// Resolves the whole dependency tree of the package supplied into `pool`
-/// and sorts them in the order they should be installed in
-/// # Arguments
-/// * `package_name` - The name of the package to resolve
-/// * `pool` - A Vec of PackageVariants where the tree gets put into
-/// * `mirrors` - A Vec of Mirrors to search for the package and dependencies
-pub fn resolve_dependencies(
-    package: Arc<PackageVariant>,
-    pool: &mut Vec<Arc<PackageVariant>>,
-    mirrors: &[Mirror],
-) -> Result<(), LError> {
-    package.get_dependencies().get_unresolved()?;
-
-    if let Some(found_package) = find_package(&package.get_name(), pool) {
-        trace!("Skipping already resolved package {}", package.get_name());
-        match found_package.get_remote()?.get_dependencies() {
-            Dependencies::Resolved(_) => return Ok(()),
-            Dependencies::Unresolved(_) => {
-                return Err(LError::new(
-                    LErrorClass::UnresolvedDependencies,
-                    format!("Package {:?}", found_package).as_str(),
-                ))
-            }
-        }
-    }
-
-    let mut new_package = package.get_remote()?.clone_to_resolved();
-    let mut new_package_dependencies: Vec<Arc<PackageVariant>> = Vec::new();
-
-    pool.push(Arc::new(PackageVariant::Remote(
-        package.get_remote()?.clone_to_resolved(),
-    )));
-
-    for dep in package.get_dependencies().get_unresolved()? {
-        trace!("Resolving dependency {} of {}", dep, package.get_name());
-        let pkg = mirror::resolve_package(dep, mirrors)?;
-        resolve_dependencies(pkg.clone(), pool, mirrors)?;
-        new_package_dependencies.push(pkg);
-    }
-
-    new_package.set_dependencies(Dependencies::Resolved(new_package_dependencies));
-    let new_package = Arc::new(PackageVariant::Remote(new_package));
-
-    let hash = &new_package.get_hash();
-    match pool.iter().position(|p| &p.get_hash() == hash) {
-        Some(pos) => {
-            trace!("Pulling back package {:?}", new_package.get_name());
-            pool.remove(pos);
-            pool.push(new_package);
-            Ok(())
-        }
-        None => Err(LError::new(
-            LErrorClass::PackageNotFound,
-            format!("Package disappeared: {}", package.get_name()).as_str(),
-        )),
-    }
+/// * `hash` - The hash to search for
+/// * `list` - The list to search
+pub fn find_package_hash<T: Package>(hash: &str, list: &[Arc<T>]) -> Option<Arc<T>> {
+    list.iter()
+        .find(|package| package.get_hash() == hash)
+        .cloned()
 }
 
 /// Deserializes a integer from a string
@@ -168,21 +90,6 @@ pub fn extract(source: &Path, destination: &Path) -> Result<(), LError> {
     archive.unpack(destination)?;
 
     Ok(())
-}
-
-/// Extracts the packages from the pool that have no dependers
-/// # Arguments
-/// * `pool` - The pool to search
-pub fn get_root_packages<T: Package>(pool: &[Arc<T>]) -> Vec<Arc<T>> {
-    let mut root_packages: Vec<Arc<T>> = Vec::new();
-
-    for package in pool {
-        if !pool.iter().any(|p| package.is_dependency_of(p.as_ref())) {
-            root_packages.push(package.clone());
-        }
-    }
-
-    root_packages
 }
 
 impl From<toml::de::Error> for LError {
---
+++ src/util/dependencies.rs
@@ -0,0 +1,96 @@
+use crate::error::*;
+use crate::mirror::*;
+use crate::package::*;
+
+/// Resolves the dependencies of the supplied package using the provided mirrors
+/// into the provided pool
+/// # Arguments
+/// * `package` - The reference to the package to process
+/// * `pool` - The pool to use for storing the packages
+/// * `mirrors` - The mirrors to search for the package
+pub fn resolve_dependencies(
+    package: PackageRef,
+    pool: &mut Vec<PackageRef>,
+    mirrors: &[Mirror],
+) -> Result<(), LError> {
+    // First of all, check if the package isn't already resolved
+    let hash = package.get_hash();
+    if pool.iter().any(|p| p.get_hash() == hash) {
+        trace!("Skipping resolved package {}", package.get_fq_name());
+        return Ok(());
+    }
+
+    // Push the package for now to prevent double resolving
+    pool.push(package.clone());
+
+    // Go through the dependencies and resolve them too
+    let pkg_fq_name = package.get_fq_name();
+    let mut new_deps: Vec<PackageRef> = Vec::new();
+    for dependency in package
+        .read()
+        .expect("Lock package mutex")
+        .get_dependencies()
+        .get_unresolved()?
+    {
+        debug!(
+            "Resolving dependency {} of package {}",
+            dependency, pkg_fq_name
+        );
+
+        // Check if the dependency does already exist
+        match pool.iter().find(|p| &p.get_name() == dependency) {
+            Some(p) => {
+                // Reuse the dependency
+                trace!("Reusing resolved dependency {}", p.get_name());
+                new_deps.push(p.clone());
+            }
+            None => {
+                // If the dependency is to be resolved, query the mirrors, resolve its dependencies and push it
+                let dependency = resolve_package(dependency, mirrors)?;
+                resolve_dependencies(dependency.clone(), pool, mirrors)?;
+                new_deps.push(dependency);
+            }
+        }
+    }
+
+    // Add the new dependencies as resolved
+    package
+        .write()
+        .expect("Lock package mutex")
+        .set_dependencies(Dependencies::Resolved(new_deps));
+
+    // Pull the package back
+    let hash = &package.get_hash();
+    match pool.iter().position(|p| &p.get_hash() == hash) {
+        Some(pos) => {
+            trace!("Pulling back package {:?}", package.get_name());
+            let pkg = pool.remove(pos);
+            pool.push(pkg);
+            Ok(())
+        }
+        None => Err(LError::new(
+            LErrorClass::PackageNotFound,
+            format!("Package disappeared: {}", package.get_name()).as_str(),
+        )),
+    }
+}
+
+/// Extracts the packages from the pool that have no dependers
+/// # Arguments
+/// * `pool` - The pool to search
+pub fn get_root_packages(pool: &[PackageRef]) -> Vec<PackageRef> {
+    let mut root_packages: Vec<PackageRef> = Vec::new();
+
+    for package in pool {
+        if !pool.iter().any(|p| {
+            package
+                .read()
+                .expect("Lock package mutex")
+                .is_dependency_of(&p.read().unwrap().clone())
+        }) {
+            root_packages.push(package.clone());
+        }
+    }
+
+    root_packages
+}
