9655b6a0e2493d042b37a548d0fd837906e8a1b7
Max Kofler <kofler.max.dev@gmail.com>
2023-05-24 21:12:57 +0200

    package::local: install() deploy() copy_to_root()
    These new functions take care of deploying new packages to a system

--- src/package/local.rs
+++ src/package/local.rs
@@ -1,13 +1,10 @@
-use std::{
-    path::{Path, PathBuf},
-    slice::Iter,
-};
+use std::path::{Path, PathBuf};
 
 use crate::{
     config::Config,
-    db::{DBConnection, DBTransaction},
+    db::DBConnection,
     error::*,
-    package::installed::InstalledPackage,
+    package::{installed::*, *},
     util::{self, fs::FSEntry},
     *,
 };
@@ -34,20 +31,107 @@ pub struct LocalPackage {
 }
 
 impl LocalPackage {
+    /// Installs this package, returns a InstalledPackage variant.
+    /// This function also ensures that the dependencies are installed first.
+    /// # Arguments
+    /// * `config` - The configuration to use
+    /// * `db_con` - The database connection to insert this package into
+    pub fn install(
+        self,
+        config: &Config,
+        db_con: &mut DBConnection,
+    ) -> Result<PackageVariant, LError> {
+        let emsg = format!("When installing package {}", self.get_fq_name());
+        warn!("TODO: Check if this package isn't already installed");
+
+        // Ensure the dependencies first
+        for dependency in self.get_dependencies().get_resolved().err_prepend(&emsg)? {
+            let mut dependency_guard = dependency.write().unwrap();
+            let dependency: PackageVariant = dependency_guard.clone();
+
+            match dependency {
+                PackageVariant::Installed(_) => {}
+                PackageVariant::Remote(_) => {
+                    return Err(LError::new(
+                        LErrorClass::UnexpectedPackageVariant,
+                        "Can't install remote package",
+                    ))
+                    .err_prepend(&emsg);
+                }
+                PackageVariant::Local(p) => {
+                    debug!(
+                        "Installing dependency {} of package {}",
+                        p.get_fq_name(),
+                        self.get_fq_name()
+                    );
+                    *dependency_guard = p.install(config, db_con)?;
+                }
+            }
+        }
+
+        usermsg!("Installing package {}", self.get_fq_name());
+        let installed_pkg = self.deploy(config, db_con)?;
+
+        debug!("Done installing");
+        Ok(PackageVariant::Installed(installed_pkg))
+    }
+
     /// Deploys this package to the system using the provided config
     /// # Arguments
     /// * `config` - The config to reference for deployment
-    pub fn deploy(&self, config: &Config) -> Result<(), LError> {
-        for dep in self.dependencies.get_resolved()? {
-            let dep = dep.get_local()?;
-            dep.deploy(config)?;
-        }
-
-        usermsg!("Deploying package {}", self.get_fq_name());
-
+    /// * `db_con` - The database connection to use for inserting this package
+    pub fn deploy(
+        self,
+        config: &Config,
+        db_con: &mut DBConnection,
+    ) -> Result<InstalledPackage, LError> {
+        debug!("Extracting package {}", self.get_fq_name());
         self.extract(config)?;
 
-        Ok(())
+        // Index the package contents
+        let mut files: Vec<FSEntry> = Vec::new();
+        debug!("Indexing package {}", self.get_fq_name());
+        let start = Instant::now();
+        files.append(&mut util::fs::index(&self.get_data_dir(config))?);
+        debug!("Took {} ms", start.elapsed().as_millis());
+
+        let transaction = db_con.new_transaction()?;
+        let installed_pkg = self.copy_to_root(config, files)?;
+
+        transaction
+            .add_package(&installed_pkg)
+            .err_prepend(&format!(
+                "When inserting package {} into database",
+                installed_pkg.get_fq_name()
+            ))?;
+
+        transaction.commit()?;
+
+        Ok(installed_pkg)
+    }
+
+    /// Copies the package contents to the new root using the supplied config
+    /// # Arguments
+    /// * `config` - The configuration to use for copying
+    /// * `files` - The vector of files to copy
+    fn copy_to_root(
+        self,
+        config: &Config,
+        files: Vec<FSEntry>,
+    ) -> Result<InstalledPackage, LError> {
+        let mut cur_src: PathBuf = self.get_data_dir(config);
+        let mut cur_dest: PathBuf = PathBuf::from(config.get_root());
+
+        // Copy the fsentries
+        let mut iter = files.iter();
+        util::fs::copy_recursive(&mut cur_src, &mut cur_dest, &mut iter).err_prepend(&format!(
+            "When copying files of package {}",
+            self.get_fq_name()
+        ))?;
+
+        let installed_pkg = InstalledPackage::from_local(self, files);
+
+        Ok(installed_pkg)
     }
 
     /// Extracts the local package into the packages_dir
